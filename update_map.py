af9426b24007c6fc7b3fd994f63e0b76485f7ffc
ENnchu <nchuen@smail.nchu.edu.tw>
2026-04-05 10:42:51 +0800

    Update update_map.py

--- update_map.py
+++ update_map.py
@@ -6,5 +6,6 @@ m = folium.Map(location=[24.1235, 120.6753], zoom_start=15)
 # 增加標記點
 folium.Marker([24.1235, 120.6753], popup="我的地點").add_to(m)
 
-# 必須存成 index.html
+# 將地圖存成 index.html 檔案
+# 這一行會把複雜的 HTML 碼寫入 index.html，覆蓋掉你原本的文字
 m.save("index.html")
